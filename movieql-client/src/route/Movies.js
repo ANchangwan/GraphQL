ba6507b1e49399db2c446fd0e13271a706d5873b
An chang wan <ssdii66@gmail.com>
2023-01-06 01:00:52 +0900

    ApolloProvieder

--- movieql-client/src/route/Movies.js
+++ movieql-client/src/route/Movies.js
@@ -1,5 +1,29 @@
+import { useApolloClient, gql } from "@apollo/client";
+import { useEffect, useState } from "react";
+
 function Movies() {
-  return <div>Movies</div>;
+  const [movies, setMovies] = useState([]);
+  const client = useApolloClient();
+  useEffect(() => {
+    client
+      .query({
+        query: gql`
+          {
+            allMovies {
+              title
+            }
+          }
+        `,
+      })
+      .then((data) => setMovies(data.data.allMovies));
+  }, [client]);
+  return (
+    <ul>
+      {movies.map((movie) => (
+        <li key={movie.id}>{movie.title}</li>
+      ))}
+    </ul>
+  );
 }
 
 export default Movies;
